d94be4a52ff23d1f6dfa41542da5dd42533d02a1
Furkan Altıntaş <furkanaltintas785@gmail.com>
2025-06-12 04:22:55 +0300

    Expertise

---
+++ components/Expertise/Expertise.jsx
@@ -0,0 +1,81 @@
+
+const Expertise = () => {
+  return (
+    <section id="expertise" className="s-expertise">
+      <div className="row s-expertise__content width-sixteen-col">
+        <div className="column xl-12 grid-block grid-section-split">
+          <div className="section-header grid-section-split__header">
+            <div className="text-pretitle">Expertise</div>
+            <h2 className="text-display-title">
+              My key areas of expertise.
+            </h2>
+            <p className="lead">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum suscipit debitis quam dignissimos veritatis atque pariatur magnam obcaecati fugit reprehenderit vel numquam facere esse est deserunt, perferendis commodi voluptatem similique.
+            </p>
+          </div>
+
+          <div className="s-expertise__content-main grid-section-split__primary">
+
+            <div className="grid-list-items list-items show-ctr">
+              <div className="grid-list-items__item list-items__item">
+                <div className="grid-list-items__title list-items__item-header">
+                  <h3 className="list-items__item-title">Digital Marketing</h3>
+                </div>
+                <div className="grid-list-items__text list-items__item-text">
+                  <p>
+                    Quibusdam quis autem voluptatibus earum vel ex error ea magni. Rerum quam quos. Aut asperiores
+                    sit mollitia. Rem neque et voluptatem eos quia sed eligendi et. Eaque velit eligendi ut
+                    magnam. Cumque ducimus laborum doloribus facere maxime vel earum quidem enim suscipit.
+                  </p>
+                </div>
+              </div>
+              <div className="grid-list-items__item list-items__item">
+                <div className="grid-list-items__title list-items__item-header">
+                  <h3 className="list-items__item-title">Project Management</h3>
+                </div>
+                <div className="grid-list-items__text list-items__item-text">
+                  <p>
+                    Rerum quam quos. Quibusdam quis autem voluptatibus earum vel ex error ea magni. Aut asperiores
+                    sit mollitia. Rem neque et voluptatem eos quia sed eligendi et. Eaque velit eligendi ut
+                    magnam. Cumque ducimus laborum doloribus facere maxime vel earum quidem enim suscipit.
+                  </p>
+                </div>
+              </div>
+              <div className="grid-list-items__item list-items__item">
+                <div className="grid-list-items__title list-items__item-header">
+                  <h3 className="list-items__item-title">Content Marketing</h3>
+                </div>
+                <div className="grid-list-items__text list-items__item-text">
+                  <p>
+                    Eaque velit eligend. iQuibusdam quis autem voluptatibus earum vel ex error ea magni.
+                    Rerum quam quos. Aut asperiores sit mollitia. Rem neque et voluptatem eos quia sed eligendi et.
+                    Eaque velit eligendi ut magnam. Cumque ducimus laborum doloribus facere maxime vel earum quidem enim suscipit.
+                  </p>
+                </div>
+              </div>
+              <div className="grid-list-items__item list-items__item">
+                <div className="grid-list-items__title list-items__item-header">
+                  <h3 className="list-items__item-title">Research & Discovery</h3>
+                </div>
+                <div className="grid-list-items__text list-items__item-text">
+                  <p>
+                    Quibusdam quis autem voluptatibus earum vel ex error ea magni. Rerum quam quos. Aut asperiores
+                    sit mollitia. Rem neque et voluptatem eos quia sed eligendi et. Eaque velit eligendi ut
+                    magnam. Cumque ducimus laborum doloribus facere maxime vel earum quidem enim suscipit.
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="s-expertise__content-btn grid-section-split__bottom">
+            <a href="services.html" className="btn btn--stroke u-fullwidth">View All Services</a>
+          </div>
+
+        </div>
+      </div>
+
+    </section>
+  )
+}
+
+export default Expertise

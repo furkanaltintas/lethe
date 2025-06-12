1d8a69fa02d279476a1ba260767980572f0691e6
Furkan Altıntaş <furkanaltintas785@gmail.com>
2025-06-12 04:22:48 +0300

    Cta

---
+++ components/Cta/Cta.jsx
@@ -0,0 +1,24 @@
+
+const Cta = () => {
+    return (
+        <section id="cta" className="s-cta">
+            <div className="row row-x-center text-center">
+                <div className="column xl-8 lg-12">
+
+                    <div className="s-cta__content">
+                        <h2 className="text-display-title">
+                            Get started with a consultation today.
+                        </h2>
+                        <p className="lead">
+                            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quis rem, esse doloribus sint eaque at debitis enim vitae minus expedita ratione dignissimos sit nostrum optio sequi. Ipsa at beatae quam.
+                        </p>
+                        <a href="contact.html" className="btn btn--primary">Let's Work Together</a>
+                    </div>
+
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default Cta
